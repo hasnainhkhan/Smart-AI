3f2b2a5c649a7382128f68fb66f62c9a2e501285
hasnainhkhan <hhk2170@gmail.com>
2025-06-01 20:30:41 +0530

    dashboard created

--- chatapp/src/test/java/com/ServiceRegistery/ChatappApplicationTests.java
+++ chatapp/src/test/java/com/ServiceRegistery/ChatappApplicationTests.java
@@ -1,13 +1,15 @@
-package com.ServiceRegistery;
+package com.ServiceRegistery;  // better to match main app package: com.serviceregistery.chatapp
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.chat.ai.ChatappApplication;
+
+@SpringBootTest(classes = ChatappApplication.class)
 class ChatappApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
